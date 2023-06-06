934a608403e8c45e4c3999083acb4074bb96dfde
Omar Guerra Alfaro <102171656+omGuerraAlfaro@users.noreply.github.com>
2023-06-06 10:39:15 -0400

    ...

--- src/components/form/form.service.ts
+++ src/components/form/form.service.ts
@@ -1,7 +1,7 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Form, Properties } from '../../models/form.model';
+import { Form } from '../../models/form.model';
 
 @Injectable()
 export class FormService {
---
+++ src/dto/form.dto.ts
@@ -0,0 +1,67 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+class GeometryDto {
+    @ApiProperty()
+    gid: number;
+
+    @ApiProperty()
+    type: string;
+
+    @ApiProperty({ type: [Number] })
+    coordinates: number[];
+}
+
+class PropertiesDto {
+    @ApiProperty()
+    propid: number;
+
+    @ApiProperty()
+    userId: string;
+
+    @ApiProperty()
+    dateTime: Date;
+
+    @ApiProperty({ nullable: true })
+    formSprinkler: string;
+
+    @ApiProperty({ nullable: true })
+    formDamage: string;
+
+    @ApiProperty({ nullable: true })
+    formHumidity: string;
+
+    @ApiProperty({ nullable: true })
+    formCompaction: string;
+
+    @ApiProperty({ nullable: true })
+    formFauna: string;
+
+    @ApiProperty({ nullable: true })
+    formCount: string;
+
+    @ApiProperty({ nullable: true })
+    formDiseases: string;
+
+    @ApiProperty({ nullable: true })
+    formGirdling: string;
+
+    @ApiProperty({ nullable: true })
+    formPlague: string;
+}
+
+export class FormDto {
+    @ApiProperty()
+    form_id: number;
+
+    @ApiProperty()
+    type: string;
+
+    @ApiProperty({ nullable: true })
+    image: string;
+
+    @ApiProperty({ type: PropertiesDto })
+    properties: PropertiesDto;
+
+    @ApiProperty({ type: GeometryDto })
+    geometry: GeometryDto;
+}
